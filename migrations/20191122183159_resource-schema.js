2add10ed17fb5d478890c3de5bf09b2bd0f6b1fb
AidenBow <AidenBow@users.noreply.github.com>
2019-11-22 21:00:39 -0800

    final edits

--- migrations/20191122183159_resource-schema.js
+++ migrations/20191122183159_resource-schema.js
@@ -1,14 +1,15 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable("resources", table => {
-    
+
     table.integer("project_id").notNullable()
       .unsigned()
       .references('ID')
       .inTable('projects')
       .onDelete('CASCADE')
       .onUpdate('CASCADE');
-    table.increments("ID").primary(['ID', 'project_id']);
+    table.increments("resource_number")
+    table.integer("ID").primary(['resource_number', 'project_id']);
     table.string("name", 64).notNullable().unique();
     table.string("description", 255);
   })
