a0d305a9e14a28ba556a46aabb25c4480d9c6714
AidenBow <AidenBow@users.noreply.github.com>
2019-12-02 19:34:47 -0800

    finished migrations

--- migrations/20191202183836_project.js
+++ migrations/20191202183836_project.js
@@ -4,10 +4,42 @@ exports.up = function(knex) {
     knex.schema
       .createTable("projects", table => {
         table.increments("ID")
+        table.string("name", 64).notNullable()
+        table.string("description", 255)
+        table.bool("completed").notNullable().defaultTo(false)
       })
 
       .createTable("resources", table => {
         table.increments("ID")
+        table.string("name", 64).notNullable().unique();
+        table.string("description", 255);
+      })
+
+      .createTable("tasks", table => {
+        table.increments("ID").primary();
+        table.integer("project_id").notNullable()
+          .unsigned()
+          .references('ID')
+          .inTable('projects')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+        table.string("description", 255).notNullable();
+        table.string("notes", 255);
+        table.bool("completed").notNullable().defaultTo(false);
+      })
+      .createTable("project_resources", table => {
+        table.integer("project_id").notNullable()
+          .unsigned()
+          .references('ID')
+          .inTable('projects')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+        table.integer("resource_id").notNullable()
+          .unsigned()
+          .references('ID')
+          .inTable('resources')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
       })
   )
 };
@@ -15,7 +47,9 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   return (
     knex.schema
-      .dropTableIfExits("projects")
-      .dropTableIfExits("projects")
+      .dropTableIfExists("projects")
+      .dropTableIfExists("resources")
+      .dropTableIfExists("tasks")
+      .dropTableIfExists("project_resources")
   )
 };
